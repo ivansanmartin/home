a439eb49b8c5b968162f4f34e879174cea8115b7
BuildTools <unconfigured@null.spigotmc.org>
2022-05-20 15:31:43 -0400

    Update

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// MENU RESPONSIVE 
+
+var btnMenuOpen = document.getElementById('btnMenuOpen'),
+    btnMenuClose = document.getElementById('btnMenuClose'),
+    menuResponsive = document.getElementById('menu-Bar'),
+    enlaces = document.getElementById('enlaces');
+
+
+    // Clic abrir
+    btnMenuOpen.addEventListener('click', () => {
+
+        menuResponsive.classList.add('active');
+
+
+
+    });
+
+    // Clic cerrar
+    btnMenuClose.addEventListener('click', () => {
+
+        menuResponsive.classList.remove('active');
+
+    });
+
+    //Cerrar menu con elementos de  enlaces
+
+    enlaces.addEventListener('click', () => {
+        menuResponsive.style.transitionDelay = '0.5s';
+        menuResponsive.classList.remove('active');
+
+    });
