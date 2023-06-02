a85568204ef8b7be5c4bde5f05527505e274e364
Iván <ivansanmartin987@gmail.com>
2023-06-02 02:00:48 -0400

    Added header navegator
    Navegator adapted to mobile users

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 
+const menuButton = document.getElementById("menu")
+const navegator = document.getElementById("navegator")
+const closeButton = document.getElementById("closeMenu")
 
+menuButton.addEventListener("click", () => {
+    navegator.classList.add("active")
+
+})
+
+closeButton.addEventListener("click", () => {
+    navegator.classList.remove("active")
+})
     
